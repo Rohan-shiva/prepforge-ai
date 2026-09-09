6be6fae9f468ab99722b03fcd93c8be9b6e52a7a
Rohan <shivarohan20@gmail.com>
2026-09-09 19:35:41 +0530

    feat: add backend package configuration and server entry point

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+const app = require('./src/app');
+const connectDB = require('./src/config/database');
+
+const invokeGeminiAI = require('./src/services/ai.service');
+// const {resume,selfDescription,jobDescription}=require("./src/services/temp");
+// const generateInterviewReport=require('./src/services/ai.service');
+
+connectDB();
+// generateInterviewReport({resume,selfDescription,jobDescription});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
